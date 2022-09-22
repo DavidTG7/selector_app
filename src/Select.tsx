12c26f2076c5c6ae2fd95fbb618c44b8ee33e4c2
DavidTG7 <davidthekoa@gmail.com>
2022-09-21 23:43:44 -0500

    Create Select component and initial styles

---
+++ src/Select.tsx
@@ -0,0 +1,31 @@
+import styles from './select.module.css'
+
+type SelectOptions = {
+  label: string
+  value: any
+}
+
+type SelectProps = {
+  options: SelectOptions[]
+  value?: SelectOptions
+  onChange: (value: SelectOptions | undefined) => void
+}
+
+export const Select = ({ value, onChange, options }: SelectProps) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <span className={styles.value}>Value</span>
+        <button className={styles['clear-btn']}>&times;</button>
+        <div className={styles.caret}></div>
+        <ul className={styles.options}>
+          {options?.map(option =>(
+            <li key={option.label} className={styles.otion}>
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
